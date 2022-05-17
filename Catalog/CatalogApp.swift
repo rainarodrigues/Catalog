6f46538b01adb336014a07d7539546f71bc29b2b
Raina Rodrigues de Lima <rainarodriguesdelima@10-45-48-131.0200.ifce.br.iosda.org>
2022-05-17 16:55:26 -0300

    first commit

---
+++ Catalog/CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogApp.swift
+//  Catalog
+//
+//  Created by Raina Rodrigues de Lima on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
